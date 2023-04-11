8b74d8493f71a1c8ad18a4204d9ccb492c0f2054
Deepan425 <deepanbalaji041@gmail.com>
2023-04-11 16:14:10 +0530

    Added today's date

---
+++ src/javascript-playground.js
@@ -0,0 +1,9 @@
+let input = ""
+
+function onclick()
+{
+  let input = "";
+  input = document.getElementById("input").innerHTML;
+  document.getElementById("id").innerHTML = input;
+}
+document.getElementById("id").innerHTML = "Wow";
